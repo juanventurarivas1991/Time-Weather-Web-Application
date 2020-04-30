22a3b3de56836734ce5a9d71a51423dc9c515862
juan <juanventurarivas@gmail.com>
2020-04-30 11:32:07 -0400

    weather.component.js

--- weather.component.js
+++ weather.component.js
@@ -2,12 +2,25 @@
 myApp.component('weatherComponent', {
     template:"<p>Weather: {{vm.weather}}</p>",
     controllerAs: 'vm',
-    controller: function WeatherController($element, $attrs) {
-      /* vm = this;
-       var apikey = "b441194a96d8642f1609a75c1441793f";
-       vm.weather = 
-       api.openweathermap.org/data/2.5/weather?q={London}&appid:{apikey};
- */ 
+    controller: function WeatherController($scope,$element, $attrs) {
+      vm = this;
+      
+
+      var apikey = "b441194a96d8642f1609a75c1441793f";
+      var city = "London";
+     
+      
+      fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + apikey)
+      .then(function(response) {
+       vm.weather = response.main.temp;
+        
+
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+      });
+    
     }
   });
 
