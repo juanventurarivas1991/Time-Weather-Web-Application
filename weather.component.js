b0ff2516e358c838c7d4167a2a811bfac6c531cd
juan <juanventurarivas@gmail.com>
2020-04-30 12:23:26 -0400

    weather[0].description

--- weather.component.js
+++ weather.component.js
@@ -1,24 +1,26 @@
 
 myApp.component('weatherComponent', {
-    template:"<p>Weather: {{vm.weather}}</p>",
+    template:"<p>Weather: {{weather}}{{text}}</p>",
     controllerAs: 'vm',
     controller: function WeatherController($scope,$element, $attrs) {
       vm = this;
-      
-
+      controller = $scope;
+      $scope.text = "hola";
+     
       var apikey = "b441194a96d8642f1609a75c1441793f";
       var city = "London";
      
       
       fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + apikey)
-      .then(function(response) {
-       vm.weather = response.main.temp;
-        
-
-        return response.json();
+      .then(function (response) { 
+        return response.json(); 
       })
-      .then(function(myJson) {
-        console.log(myJson);
+      .then(function(response) {
+        console.log(response);
+        $scope.vm.text ="hola mundo";
+        $scope.weather = response.weather[0].description;
+        console.log($scope.vm.weather);
+        controller.text ="mundo" ;
       });
     
     }
